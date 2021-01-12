febe870e2de3b59a8e211c8a8b3e6b96ab78a837
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2021-01-12 12:09:27 +0100

    deux

---
+++ entete.php
@@ -0,0 +1,5 @@
+<header>
+ <div id="titre_principal">
+ <h1>Ma page</h1>
+ </div>
+ </header>
--- index.php
+++ index.php
@@ -1,14 +1,27 @@
-<?php /* Le code PHP se met ici */ ?>
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html>
+ <head>
+ <meta charset="utf-8" />
+ <link rel="stylesheet" href="style.css" />
+ <title>mypage</title>
+ </head>
+ <body>
+ <div id="bloc_page">
+     
+ <!-- L'en-tête -->
+ <?php include("entete.php"); ?>
+ <!-- Le corps -->
+ <section>
+ <?php include("menu.php"); ?>
+ <article>
+ <p>Article </p>
+ </article>
+ <aside>
+ <p>Aside </p>
+ </aside>
+ </section>
+ <!-- Le pied de page -->
+ <?php include("pied_de_page.php"); ?>
+ </div>
+ </body>
 </html>
-
-
